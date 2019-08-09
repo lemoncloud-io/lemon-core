55a59659e7e94384fc7e8b291bccb1e71c1a0bc7
Steve <steve@lemoncloud.io>
2019-08-09 13:17:26 +0900

    project files

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-use-before-define': 'off'
+    },
+};
