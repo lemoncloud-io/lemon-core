d414ba505e6e99ace7399b53642a5f3ee57627e6
Steve <steve@lemoncloud.io>
2020-02-04 17:11:20 +0900

    fix: web response of isBase64Encoded

--- src/cores/lambda/lambda-notification-handler.spec.ts
+++ src/cores/lambda/lambda-notification-handler.spec.ts
@@ -49,7 +49,7 @@ describe('LambdaNotificationHandler', () => {
         const snsTopicArn ='arn:aws:sns:ap-northeast-2:796730245826:lemon-todaq-out';
         const subscribeURL = 'https://sns.ap-northeast-2.amazonaws.com/?Action=ConfirmSubscription&TopicArn=arn:aws:sns:ap-northeast-2:796730245826:lemon-todaq-out&Token=.....';
         const signature = 'E/CqqnBfEbglCBM4W0uGTN4IrPM/40MqyOrVUHvsZPI5EDpfDj8L/UTUiu+y3pUoY/b//EJB0lXsy+VcjUBPa66n93/c4YqhrTJxq2zlWHe0rEUQ9GJVqDXQATNflO1zKTRQonRee0kVksB2PP+1vy43gRWs/1QD5QvfrqpUz7mFlB0jyQ4bCm4b6b5iIkBeh0P0A2InuE0BOl9dcS2qNWCeThAU88aNSWaPeTcY0LnQm1szUYNouphUhxf5ogKL9Qv0hhfOC4mGpqw6IJ9PTxY6Ff3UlFeNla/Z73AOTVzhrmcs7ih2ycIYJqedUGqILQL5IdDsHcJpzl41aWno7Q==';
-        expect2(res).toEqual({"body": "NOP", "headers": {"Access-Control-Allow-Credentials": true, "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain; charset=utf-8"}, "statusCode": 200});
+        expect2(res).toEqual({"body": "NOP", "headers": {"Access-Control-Allow-Credentials": true, "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain; charset=utf-8" }, statusCode: 200, isBase64Encoded:false });
         expect2(data, 'id').toEqual({ id: event.path });
         //! param should have `subscribeURL` for `SubscriptionConfirmation`
         expect2(data, 'param').toEqual({ param:{ snsMessageId:'ef9765be-a053-40d8-907b-4a212b8a8b6e', snsMessageType:'SubscriptionConfirmation', snsTopicArn, snsSubscriptionArn, subscribeURL, signature }});
@@ -77,7 +77,7 @@ describe('LambdaNotificationHandler', () => {
             return 'NOP';
         })
         const res = await lambda.handle(event, null);
-        expect2(res).toEqual({"body": "NOP", "headers": {"Access-Control-Allow-Credentials": true, "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain; charset=utf-8"}, "statusCode": 200});
+        expect2(res).toEqual({"body": "NOP", "headers": {"Access-Control-Allow-Credentials": true, "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain; charset=utf-8" }, statusCode: 200, isBase64Encoded:false });
         expect2(data, 'id').toEqual({ id: event.path });
         expect2(data, 'param').toEqual({ param:{ snsMessageId:'ce739c18-f6bf-5d16-8818-ca6a56522dfa', snsMessageType:'Notification', snsTopicArn:'arn:aws:sns:ap-northeast-2:796730245826:lemon-todaq-out', snsSubscriptionArn:'arn:aws:sns:ap-northeast-2:796730245826:lemon-todaq-out:691a3ee3-5e1b-4b44-b0c0-ad1cd6a5b87f' }});
         expect2(data, 'context').toEqual({ context:{ accountId:'796730245826', clientIp:'54.239.110.00', domain:'9tdk25wjpd.execute-api.ap-northeast-2.amazonaws.com', requestId:'3e7f3eff-f666-4e4f-9d47-590418938684' }});
@@ -108,7 +108,7 @@ describe('LambdaNotificationHandler', () => {
         const snsTopicArn = 'arn:aws:sns:ap-northeast-2:796730245826:lemon-todaq-out';
         const subject = 'Hello Noti';
         const myParam = { a:'b', b:1 };
-        expect2(res).toEqual({"body": "NOP", "headers": {"Access-Control-Allow-Credentials": true, "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain; charset=utf-8"}, "statusCode": 200});
+        expect2(res).toEqual({"body": "NOP", "headers": {"Access-Control-Allow-Credentials": true, "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain; charset=utf-8"}, statusCode: 200, isBase64Encoded: false});
         expect2(data, 'id').toEqual({ id: event.path });
         //! myParam should be merged to handler's param.
         expect2(data, 'param').toEqual({ param:{ snsMessageId:'ce739c18-f6bf-5d16-8818-ca6a56522dfa', snsMessageType:'Notification', snsTopicArn, snsSubscriptionArn, subject, ...myParam }});
