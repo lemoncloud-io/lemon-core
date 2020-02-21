5f68120c14ffded0d6d9158e8cf4a838c419b93a
tyler <tyler@lemoncloud.io>
2020-02-21 13:54:15 +0900

    Feat: aws-s3-service for json

--- src/cores/aws/aws-s3-service.ts
+++ src/cores/aws/aws-s3-service.ts
@@ -13,47 +13,64 @@
  *  Common Headers
  ** ****************************************************************************************************************/
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { $engine, $U, _log, _inf, _err } from '../../engine';
+import { $engine, $U, _log, _inf, _err, doReportError } from '../../engine';
 const NS = $U.NS('S3', 'blue');
 
 import AWS from 'aws-sdk';
-import { v4 } from 'uuid';
 import * as StringTool from 'string_decoder';
 import { CoreServices } from '../core-services';
+import { DummyStorageService } from '../storage-service';
+
+/**
+ * S3를 NoSql을 대체해서 사용하기 위한 서비스
+ */
+export interface CoreS3Service extends CoreServices {
+    name(): string;
+    hello(): string;
+    read(id: string): Promise<S3Model>;
+    save(id: string, body: S3Model, meta?: Metadata, tags?: TagSet): Promise<S3Model>;
+    delete(id: string): Promise<S3Model>;
+    update(id: string, body: S3Model, meta?: Metadata, tags?: TagSet): Promise<S3Model>;
+}
+
+class S3CoreService {
+    protected type: string;
+    public constructor() {}
+
+    public asKey = (id: string): string => {
+        const prefix = id.match(/\//) !== null ? `${id}` : `${this.type || ''}/${id}`;
+        const _id = id.match(/\.json/) !== null ? `${prefix}.json` : `${prefix}`;
+        return _id;
+    };
+}
+
+export interface S3Model {
+    id?: string;
+    type?: string;
+    stereo?: string;
+    syncId?: string;
+}
+
+export interface S3Params {
+    Bucket: string;
+    Key: string;
+    Body?: string;
+    ContentType?: string;
+    Metadata?: Metadata;
+}
 
 export interface TagSet {
     [key: string]: string;
 }
 
-export interface CoreS3Service extends CoreServices {
-    bucket: (target?: string) => Promise<string>;
-    putObject: (
-        body: string,
-        fileName?: string,
-        contentType?: string,
-        tags?: TagSet,
-    ) => Promise<{ Bucket: string; Key: string; Location: string }>;
-    getObject: (fileName: string) => Promise<any>;
-    getDecodedObject: (fileName: string) => Promise<any>;
+export interface Metadata {
+    [key: string]: string;
 }
 
 /** ****************************************************************************************************************
  *  Public Instance Exported.
  ** ****************************************************************************************************************/
 const region = (): string => $engine.environ('REGION', 'ap-northeast-2') as string;
-
-/**
- * use `target` as value or environment value.
- * environ('abc') => string 'abc'
- * environ('ABC') => use `env.ABC`
- */
-const environ = (target: string, defEnvName: string, defEnvValue: string) => {
-    const isUpperStr = target && /^[A-Z][A-Z0-9_]+$/.test(target);
-    defEnvName = isUpperStr ? target : defEnvName;
-    const val = defEnvName ? ($engine.environ(defEnvName, defEnvValue) as string) : defEnvValue;
-    target = isUpperStr ? '' : target;
-    return `${target || val}`;
-};
 
 //! get aws client for S3
 const instance = async () => {
@@ -62,13 +79,24 @@ const instance = async () => {
     return new AWS.S3(config); // SQS Instance. shared one???
 };
 
-//! main service instance.
-export class AWSS3Service implements CoreS3Service {
+/**
+ * JSON 타입 저장용 S3 Storage
+ */
+export class AWSS3Service extends S3CoreService implements CoreS3Service {
     /**
      * environ name config.
      */
     public static ENV_S3_NAME = 'MY_S3_BUCKET';
     public static DEF_S3_BUCKET = 'lemon-hello-www';
+    protected bucket: string;
+    protected type: string;
+    protected idName: string;
+    public constructor(type?: string, idName?: string) {
+        super();
+        this.bucket = $U.env(AWSS3Service.ENV_S3_NAME, AWSS3Service.DEF_S3_BUCKET);
+        this.type = type || 'test';
+        this.idName = idName || '_id';
+    }
 
     /**
      * get name of this
@@ -77,18 +105,12 @@ export class AWSS3Service implements CoreS3Service {
 
     /**
      * hello
+     * @return {string} `target_service`:`which_bucket`:`this_type`
      */
-    public hello = () => `aws-s3-service:${''}`;
+    public hello = () => `aws-s3-service:${this.bucket}:${this.type}`;
 
     /**
-     * get target endpoint by name.
-     */
-    public bucket = async (target?: string) => {
-        return environ(target, AWSS3Service.ENV_S3_NAME, AWSS3Service.DEF_S3_BUCKET);
-    };
-
-    /**
-     * upload a file to S3 Bucket
+     * upload a json file to S3 Bucket
      *
      * ```js
      * const res = $s3s.putObject(JSON.stringify({ message }), 'test.json', 'application/json');
@@ -102,24 +124,28 @@ export class AWSS3Service implements CoreS3Service {
      * }
      * ```
      *
-     * @param {string} bucketId
-     * @param {string} fileName
-     * @param {string} body
+     * @param {string} id
+     * @param {object} body
+     * @param {object} meta             (optional) meta to save.
      * @param {object} tags             (optional) tags to save.
      */
-    public putObject = async (body: string, fileName?: string, contentType?: string, tags?: TagSet) => {
+    public save = async (id: string, body: S3Model, meta?: Metadata, tags?: TagSet) => {
+        _log(NS, `aws-s3-save:${this.bucket}(${id || ''})...`);
+        if (!id) throw new Error('@id is required!');
         if (!body) throw new Error('@body is required!');
-        _log(NS, `putObject(${fileName || ''})...`);
-        //! get unique file name.
-        fileName = fileName || `${this.nextId()}.json`;
-        contentType = contentType || 'application/json';
+        if (!(typeof body == 'object')) throw new Error('@body must be Object');
+        if (meta && !(typeof meta == 'object')) throw new Error('@meta must be Object');
 
-        const Bucket = await this.bucket();
-        const params = { Bucket, Key: fileName, Body: body, ContentType: 'text/plain; charset=utf-8' };
+        const Bucket = this.bucket || '';
+        const _id = this.asKey(id);
+        const Body = JSON.stringify({ ...body });
+        const params: S3Params = { Bucket, Key: _id, Body, ContentType: 'application/json', Metadata: meta };
         const options = {
             tags: undefined as any,
         };
-        if (contentType) params.ContentType = contentType;
+        _log(NS, `> S3.params = ${JSON.stringify(params)}`);
+        options && _log(NS, `> S3.options = ${JSON.stringify(options)}`);
+
         if (tags && typeof tags == 'object') {
             options.tags = Object.keys(tags).reduce((L, key) => {
                 const val = tags[key];
@@ -131,55 +157,29 @@ export class AWSS3Service implements CoreS3Service {
         return instance()
             .then(_ => _.upload(params, options).promise())
             .then(data => {
-                const Key = (data && data.Key) || fileName;
-                const Location = (data && data.Location) || '';
+                const Key = (data && data.Key) || id;
+                // const Location = (data && data.Location) || '';
                 _log(NS, `> data[${Bucket}].Key =`, Key);
-                return { Bucket, Key, Location, saved: body };
+                return { ...body };
             })
             .catch(e => {
-                _err(NS, `! err[${Bucket}]=`, e);
-                throw e;
-            });
-    };
-
-    /**
-     * get a file from S3 Bucket
-     *
-     * @param {string} bucketId
-     * @param {string} fileName
-     */
-    public getObject = async (fileName: string): Promise<any> => {
-        if (!fileName) throw new Error('@fileName is required!');
-
-        const Bucket = await this.bucket();
-        const params = { Bucket, Key: fileName };
-
-        //! call s3.getObject.
-        // _log(NS, '> params =', params);
-        return instance()
-            .then(_ => _.getObject(params).promise())
-            .then(data => {
-                _log(NS, '> data.type =', typeof data);
-                return data as any;
-            })
-            .catch(e => {
-                _err(NS, '! err=', e);
-                throw e;
+                doReportError(new Error(`S3:${this.bucket}:${_id} 500 SERVER`), null, null, e);
+                throw new Error(`500 SERVER - ${JSON.stringify({ code: e.code, messag: e.message })}`);
             });
     };
 
     /**
      * return decoded Object from bucket file.
-     * @param {string} fileName ex) 'hello-0001.json' , 'dits/hello-0001.json
+     * @param {string} id primary_id of Object
      */
-    public getDecodedObject = async (fileName: string): Promise<any> => {
-        if (!fileName) throw new Error('@fileName is required!');
+    public read = async (id: string) => {
+        _log(NS, `aws-s3-read:${this.bucket}(${id || ''})...`);
+        if (!id) throw new Error('@id is required!');
+        const Bucket = this.bucket || '';
+        const _id = this.asKey(id);
+        const params: S3Params = { Bucket, Key: _id };
+        _log(NS, `> S3.params = ${JSON.stringify(params)}`);
 
-        const Bucket = await this.bucket();
-        const params = { Bucket, Key: fileName };
-
-        //! call s3.getObject.
-        // _log(NS, '> params =', params);
         return instance()
             .then(_ => _.getObject(params).promise())
             .then((data: any) => {
@@ -189,15 +189,132 @@ export class AWSS3Service implements CoreS3Service {
                 return JSON.parse(decoder.write(buf));
             })
             .catch(e => {
-                _err(NS, '! err=', e);
-                throw e;
+                if (e.code == 'NoSuchKey') throw new Error(`404 NOT FOUND - ${this.idName}:${this.type}/${id}.json`);
+                else {
+                    doReportError(new Error(`S3:${this.bucket}:${_id} 500 SERVER`), null, null, e);
+                    throw new Error(`500 SERVER - ${JSON.stringify({ code: e.code, messag: e.message })}`);
+                }
+            });
+    };
+
+    protected async readSafe(id: string): Promise<S3Model> {
+        return this.read(id).catch(e => {
+            if (`${e.message || e}`.startsWith('404 NOT FOUND')) {
+                // eslint-disable-next-line @typescript-eslint/no-object-literal-type-assertion
+                const $org: S3Model = ({ [this.idName]: id } as unknown) as S3Model;
+                return $org;
+            }
+            throw e;
+        });
+    }
+
+    /**
+     * delete object from S3
+     * @param {string} id object id
+     */
+    public delete = async (id: string) => {
+        _log(NS, `aws-s3-delete:${this.bucket}(${id || ''})...`);
+        if (!id) throw new Error('@id is required!');
+        const Bucket = this.bucket || '';
+        const _id = this.asKey(id);
+        const params: S3Params = { Bucket, Key: _id };
+        _log(NS, `> S3.params = ${JSON.stringify(params)}`);
+
+        const deleteObj = await instance()
+            .then(_ => _.getObject(params).promise())
+            .then((data: any) => {
+                _log(NS, '> data.type =', typeof data);
+                const decoder = new StringTool.StringDecoder('UTF8');
+                const buf = Buffer.from(data.Body || data.Body.data || '');
+                return JSON.parse(decoder.write(buf));
+            })
+            .catch(e => {
+                if (e.code == 'NoSuchKey') throw new Error(`404 NOT FOUND - ${this.idName}:${this.type}/${id}.json`);
+                else {
+                    doReportError(new Error(`S3:${this.bucket}:${_id} 500 SERVER`), null, null, e);
+                    throw new Error(`500 SERVER - ${JSON.stringify({ code: e.code, messag: e.message })}`);
+                }
+            });
+
+        return instance()
+            .then(_ => _.deleteObject(params).promise())
+            .then(_ => {
+                return { ...deleteObj };
+            })
+            .catch(e => {
+                if (e.code == 'NoSuchKey') throw new Error(`404 NOT FOUND - ${this.idName}:${this.type}/${id}.json`);
+                else {
+                    doReportError(new Error(`S3:${this.bucket}:${_id} 500 SERVER`), null, null, e);
+                    throw new Error(`500 SERVER - ${JSON.stringify({ code: e.code, messag: e.message })}`);
+                }
             });
     };
 
     /**
-     * get next unique-id.
+     * update object from S3
+     * @param {string} id
+     * @param {object} body
+     * @param {object} meta             (optional) meta to save.
+     * @param {object} tags             (optional) tags to save.
      */
-    public nextId = () => {
-        return v4();
+    public update = async (id: string, body: S3Model, meta?: Metadata, tags?: TagSet) => {
+        _log(NS, `aws-s3-update:${this.bucket}(${id || ''})...`);
+        if (!id) throw new Error('@id is required!');
+        if (!body) throw new Error('@body is required!');
+        if (!(typeof body == 'object')) throw new Error('@body must be Object');
+        if (meta && !(typeof meta == 'object')) throw new Error('@meta must be Object');
+
+        const Bucket = this.bucket || '';
+        const _id = this.asKey(id);
+        const params: S3Params = { Bucket, Key: _id };
+
+        const selectObj = await this.readSafe(_id);
+        params.Body = JSON.stringify({ ...selectObj, ...body });
+        params.ContentType = 'application/json';
+        params.Metadata = meta;
+
+        const options = {
+            tags: undefined as any,
+        };
+
+        if (tags && typeof tags == 'object') {
+            options.tags = Object.keys(tags).reduce((L, key) => {
+                const val = tags[key];
+                L.push({ Key: key, Value: `${val}` });
+                return L;
+            }, []);
+        }
+
+        return instance()
+            .then(_ => _.upload(params, options).promise())
+            .then(data => {
+                const Key = (data && data.Key) || id;
+                _log(NS, `> data[${Bucket}].Key =`, Key);
+                return { ...body };
+            })
+            .catch(e => {
+                _err(NS, `! err[${Bucket}]=`, e);
+                throw new Error(`500 SERVER - ${JSON.stringify({ code: e.code, messag: e.message })}`);
+            });
     };
+}
+
+export class AWSS3DummyService<T extends S3Model> extends S3CoreService implements CoreS3Service {
+    protected storage: DummyStorageService<T>;
+    protected target: string;
+    protected type: string;
+    public constructor(target?: string, type?: string, idName?: string) {
+        super();
+        this.target = target;
+        this.storage = new DummyStorageService<T>(target, type, `${idName || '_id'}`);
+        this.type = type || 'dummy';
+    }
+    public name = () => `dummy`;
+    public hello = () => this.storage.hello();
+    public save = async (id: string, body: T, meta?: Metadata, tags?: TagSet): Promise<any> =>
+        this.storage.save(`${this.type}/${id}.json`, body);
+    public read = async (id: string): Promise<any> => this.storage.read(`${this.type}/${id}.json`);
+    public delete = async (id: string): Promise<any> => this.storage.delete(`${this.type}/${id}.json`);
+    public update = async (id: string, body: T, meta?: Metadata, tags?: TagSet): Promise<any> =>
+        this.storage.update(`${this.type}/${id}.json`, body);
 }
