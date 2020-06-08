2dc20e234af4050c60574a397c4f1e67a6124688
Tim <tim@lemoncloud.io>
2020-06-08 11:37:10 +0900

    refactor: model-manager
    - simplify implementation
    - add more doc

--- src/cores/model-manager.ts
+++ src/cores/model-manager.ts
@@ -21,8 +21,8 @@ const NS = $U.NS('MMGR', 'cyan'); // NAMESPACE TO BE PRINTED.
 
 /**
  * class: `AbstractManager`
- *  - abstract model manager to cover all models extending CoreModel.
- *  - handle 'name' like unique value in same type.
+ *  - abstract model manager to cover all models which extend CoreModel.
+ *  - feature to handle 'name' like unique value in same type.
  *  - typed-storage based.
  * @abstract
  */
@@ -36,6 +36,13 @@ export abstract class AbstractManager<
     public readonly storage: TypedStorageService<T, ModelType>;
     public readonly $unique: UniqueFieldManager<T, ModelType>;
 
+    /**
+     * default constructor
+     * @param type          model type string
+     * @param parent        service instance which implements StorageMakeable interface
+     * @param fields        list of model field names
+     * @param uniqueField   (optional) unique field name
+     */
     protected constructor(type: ModelType, parent: S, fields: string[], uniqueField?: string) {
         super(fields);
         this.type = type;
@@ -45,23 +52,34 @@ export abstract class AbstractManager<
     }
 
     /**
-     * prepare default-model when creation
-     * @param $def  base-model
-     * @abstract
-     */
-    protected abstract prepareDefault($def: T): T;
-
-    /**
      * hello of this service-identity
      */
     public hello = () => `${this.type}/${this.storage.hello()}`;
 
     /**
-     * default implementation of preparing model
+     * prepare default model when creation
+     *  - sub classes should implement this method
+     * @param $def  default model
+     * @abstract
+     */
+    protected abstract prepareDefault($def: T): T;
+
+    /**
+     * callback invoked just before the model is saved
+     *  - override this to customize default behavior to perform model validation, cleansing or normalization
+     * @param model     model object
+     * @param origin    original model currently in the storage
+     */
+    public onBeforeSave(model: T, origin?: T): T {
+        return super.onBeforeSave(model, origin);
+    }
+
+    /**
+     * default implementation of 'preparing' model
      *  - prepare model w/ safe creation
      * @param id        model-id to prepare
      * @param $def      default model value
-     * @param isCreate  (optional) flag to throw if 404
+     * @param isCreate  (optional) flag to allow creating a new model (default: true)
      */
     public async prepare(id: string, $def?: T, isCreate = true): Promise<T> {
         _inf(NS, `prepare(${id})...`);
@@ -69,30 +87,28 @@ export abstract class AbstractManager<
         _log(NS, `> isCreate =`, isCreate);
         if (!id) throw new Error(`404 NOT FOUND - id is not valid!`);
 
-        // if 'isCreate' flag is set, read existing model or create initial model by calling abstract method
+        // if 'isCreate' flag is set, read existing model or create initial model by calling abstract method 'prepareDefault'
         if (isCreate) return this.storage.readOrCreate(id, this.prepareDefault($def));
         // this can throw 404
         return this.storage.read(id);
     }
 
     /**
-     * default implementation of creating model
-     *  - create message model w/ parent relationship.
-     * @param model initial model object
+     * default implementation of 'inserting' model
+     * @param model model object
      */
     public async insert(model: T): Promise<T> {
         _inf(NS, `insert...`);
         if (!model) throw new Error(`@model (${this.type}-model) is required!`);
-        if (model.id) throw new Error('.id is not valid!');
 
         const $def = this.prepareDefault(null);
         const created = await this.storage.insert($def);
         const id = `${(created && created.id) || ''}`;
-        if (!id) throw new Error('.id (string) is required - insert() fail!');
+        if (!id) throw new Error('.id (string) is missing - insert() failed!');
         _log(NS, `> model.base =`, $U.json(model));
 
         const $saves = { ...model }; // clone
-        delete $saves.id; // clear id
+        delete $saves.id; // ensure there is no 'id' field
         const saved = await this.storage.save(id, $saves);
         _log(NS, `> model.saved =`, $U.json(saved));
 
@@ -100,7 +116,7 @@ export abstract class AbstractManager<
     }
 
     /**
-     * default implementation of retrieving model
+     * default implementation of 'retrieving' model
      * @param id    model id
      */
     public async retrieve(id: string): Promise<T> {
@@ -117,50 +133,35 @@ export abstract class AbstractManager<
      * default implementation of updating model
      * @param id    model id
      * @param model model object
-     * @param $inc  incremental set
+     * @param $inc  (optional) incremental set
      */
     public async update(id: string, model: T, $inc?: T): Promise<T> {
         _inf(NS, `update(${id})...`);
-        if (!id) throw new Error(`@id is required!`);
-        if (!model) throw new Error(`@model (${model.type}) is required!`);
-
-        const $org = await this.retrieve(id);
-        _log(NS, `> model.org =`, $U.json(model));
-        const $ups = this.onBeforeSave({ ...model }, $org);
-        const updated = Object.keys($ups).length ? await this.storage.update(id, $ups, { ...$inc }) : {};
-        _log(NS, `> model.updated =`, $U.json(updated));
-
-        return { ...$org, ...updated, id };
+        return this._updateModel(id, model, $inc, false);
     }
 
     /**
-     * default implementation of upserting model
+     * default implementation of 'upserting' model
      * @param id    model id
      * @param model model object
+     * @param $inc  (optional) incremental set
      */
-    public async updateOrCreate(id: string, model: T): Promise<T> {
+    public async updateOrCreate(id: string, model: T, $inc?: T): Promise<T> {
         _inf(NS, `updateOrCreate(${id})...`);
-        if (!id) throw new Error(`@id is required!`);
-        if (!model) throw new Error(`@model (${model.type}) is required!`);
-
-        const $org = await this.prepare(id, null, true);
-        const $ups = this.onBeforeSave({ ...model }, $org);
-        const updated = Object.keys($ups).length ? await this.storage.update(id, $ups) : {};
-        _log(NS, `> model.updated =`, $U.json(updated));
-
-        return { ...$org, ...updated, id };
+        return this._updateModel(id, model, $inc, true);
     }
 
     /**
      * default implementation of deleting model
-     * @param id
-     * @param destroy
+     * @param id        model id
+     * @param destroy   flag for hard delete or soft delete (by setting 'deletedAt' field)
      */
     public async delete(id: string, destroy: boolean = true): Promise<T> {
         _inf(NS, `delete(${id})...`);
         if (!id) throw new Error(`@id is required!`);
 
-        const $org = await this.retrieve(id);
+        const $org = await this.prepare(id, null, false);
+        _log(NS, `> model.org =`, $U.json($org));
         const deleted = await this.storage.delete(id, destroy);
         _log(NS, `> model.deleted =`, $U.json(deleted));
 
@@ -168,12 +169,23 @@ export abstract class AbstractManager<
     }
 
     /**
-     * callback invoked just before model is saved
-     *  - override this to customize behavior
-     * @param model
-     * @param origin
+     * internal implementation for 'update' and 'updateOrCreate'
+     * @param id        model id
+     * @param model     model object
+     * @param $inc      (optional) incremental set
+     * @param isCreate  (optional) flag to allow creating a new model (default: true)
+     * @private
      */
-    public onBeforeSave(model: T, origin?: T): T {
-        return super.onBeforeSave(model, origin);
+    private async _updateModel(id: string, model: T, $inc?: T, isCreate = true): Promise<T> {
+        if (!id) throw new Error(`@id is required!`);
+        if (!model) throw new Error(`@model (${model.type}) is required!`);
+
+        const $org = await this.prepare(id, null, isCreate);
+        _log(NS, `> model.org =`, $U.json($org));
+        const $ups = { ...model };
+        const updated = await this.storage.update(id, $ups, $inc);
+        _log(NS, `> model.updated =`, $U.json(updated));
+
+        return { ...$org, ...updated, id };
     }
 }
