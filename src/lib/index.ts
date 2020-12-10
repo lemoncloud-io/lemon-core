3b140065f6185b97a4008ad94ffecf5f11853579
Steve <steve@lemoncloud.io>
2020-12-10 23:25:12 +0900

    chore: cleanup export

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+/**
+ * `lib/index.ts`
+ * - shared libraries.
+ *
+ * @author      Steve Jung <steve@lemoncloud.io>
+ * @date        2020-12-10 export * as lib
+ *
+ * @copyright (C) lemoncloud.io 2019 - All Rights Reserved.
+ */
+// export * from './dynamo/'; //WARN - this dynamo is only for internal processing. so no need to export.
+export * from './dynamodb-value';
