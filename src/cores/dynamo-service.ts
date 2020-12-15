95181342f759a2111856e4f05675a491bf2927f0
Steve <steve@lemoncloud.io>
2020-12-15 15:35:51 +0900

    fix: dynamo value

--- src/cores/dynamo-service.ts
+++ src/cores/dynamo-service.ts
@@ -96,6 +96,11 @@ export class DynamoService<T extends GeneralItem> {
     }
 
     /**
+     * export to test..
+     */
+    public static normalize = normalize;
+
+    /**
      * prepare `CreateTable` payload.
      *
      * @param ReadCapacityUnits
@@ -238,7 +243,7 @@ export class DynamoService<T extends GeneralItem> {
                 if (key === idName || key === sortName) return memo;
                 const key2 = norm(key);
                 value = normalize(value);
-                if (value.setIndex && Array.isArray(value.setIndex)) {
+                if (value && Array.isArray(value.setIndex)) {
                     //! support set items in list
                     value.setIndex.forEach(([idx, value]: [number, string | number], seq: number) => {
                         if (idx !== undefined && value !== undefined) {
@@ -247,7 +252,7 @@ export class DynamoService<T extends GeneralItem> {
                             memo.UpdateExpression.SET.push(`#${key2}[${idx}] = :${key2}_${seq}_`);
                         }
                     });
-                } else if (value.removeIndex && Array.isArray(value.removeIndex)) {
+                } else if (value && Array.isArray(value.removeIndex)) {
                     //! support removing items from list
                     value.removeIndex.forEach((idx: number) => {
                         if (idx !== undefined) {
