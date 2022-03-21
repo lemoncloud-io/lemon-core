0c46cc23e9df9fed9b33f0bb60de34b059755da2
Albert <albert@lemoncloud.io>
2022-03-21 17:27:22 +0900

    style: disable eslint rules. no-inferrable-types, no-empty-function

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/camelcase': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
-        '@typescript-eslint/no-use-before-define': 'off'
+        '@typescript-eslint/no-use-before-define': 'off',
+        // Added after upgrade eslint v8
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
     },
 };
