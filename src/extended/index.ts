afc29e5ec1830b2d8f2568363845969135d34baa
Steve <steve@lemoncloud.io>
2022-03-31 20:32:35 +0900

    feat: abstract-service

---
+++ src/extended/index.ts
@@ -0,0 +1,10 @@
+/**
+ * `extended`
+ * - extended services to build abstract backend-service
+ *
+ * @author      Steve Jung <steve@lemoncloud.io>
+ * @date        2022-03-30 initial commit
+ *
+ * @copyright (C) lemoncloud.io 2022 - All Rights Reserved.
+ */
+export * from './abstract-service';
