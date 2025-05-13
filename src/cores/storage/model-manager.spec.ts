5eacf8994fac6bd95f0545d2c66f7656f55908f7
Steve Jung <steve@lemoncloud.io>
2025-05-13 21:51:25 +0900

    chore: opt spec storage

--- src/cores/storage/model-manager.spec.ts
+++ src/cores/storage/model-manager.spec.ts
@@ -18,8 +18,7 @@ import {
 import { AbstractManager } from './model-manager';
 import { expect2, GETERR } from '../../common/test-helper';
 
-//-------------------------
-//! internal definitions
+//* internal definitions
 type UserType = 'user';
 interface User extends CoreModel<UserType> {
     id?: string;
@@ -73,8 +72,7 @@ class UserManager extends AbstractManager<User, DummyStorageMaker, UserType> {
     }
 }
 
-//-------------------------
-//! create service instance.
+//* create service instance.
 export const instance = (table?: string, time?: number) => {
     table = `${table || ''}` || 'dummy-user-data.yml';
     time = time || Date.now();
@@ -86,52 +84,92 @@ export const instance = (table?: string, time?: number) => {
 
 //! main test body.
 describe('ModelManager', () => {
-    //! test w/ service
-    it('should pass identity and basic functions', async done => {
+    //* test w/ service
+    it('should pass identity and basic functions', async () => {
         const { manager, current, table } = instance();
 
-        /* eslint-disable prettier/prettier */
         expect2(() => table).toBe('dummy-user-data.yml');
-        expect2(manager.hello()).toEqual('typed-storage-service:user/proxy-storage-service:dummy-storage-service:dummy-user-data/id');
+        expect2(manager.hello()).toEqual(
+            'typed-storage-service:user/proxy-storage-service:dummy-storage-service:dummy-user-data/id',
+        );
         expect2(manager.parent.hello()).toEqual('dummy-storage-maker:TT/dummy-user-data.yml');
         expect2(manager.type).toBe('user');
         expect2(manager.current).toBe(current);
-
-        done();
     });
 
-    it('should pass read existing model(s)', async done => {
+    it('should pass read existing model(s)', async () => {
         const { manager } = instance();
-        const expected = { ns: 'TT', type: 'user', id: '1', uid: 'U0001', name: '홍길동', age: 32, contact: '010-1234-4321', active: 1 };
+        const expected = {
+            ns: 'TT',
+            type: 'user',
+            id: '1',
+            uid: 'U0001',
+            name: '홍길동',
+            age: 32,
+            contact: '010-1234-4321',
+            active: 1,
+        };
         // read from dummy file
         expect2(await manager.retrieve('1').catch(GETERR)).toEqual(expected);
-        done();
     });
 
-    it('should pass prepare existing and new model(s)', async done => {
+    it('should pass prepare existing and new model(s)', async () => {
         const { manager, current } = instance();
-        const expected = { ns: 'TT', type: 'user', id: '1', uid: 'U0001', name: '홍길동', age: 32, contact: '010-1234-4321', active: 1 };
+        const expected = {
+            ns: 'TT',
+            type: 'user',
+            id: '1',
+            uid: 'U0001',
+            name: '홍길동',
+            age: 32,
+            contact: '010-1234-4321',
+            active: 1,
+        };
         // prepare existing model
         expect2(await manager.prepare('1', {}, false).catch(GETERR), '!_id').toEqual(expected);
         expect2(await manager.prepare('1', {}, true).catch(GETERR), '!_id').toEqual(expected);
         // prepare new model (default value should be set)
         expect2(await manager.prepare('2', {}, false).catch(GETERR), '!_id').toEqual('404 NOT FOUND - user:2');
-        expect2(await manager.prepare('2', {}, true).catch(GETERR), '!_id').toEqual({ ns: 'TT', type: 'user', id: '2', active: 1, createdAt: current, updatedAt: current, deletedAt: 0 });
-        done();
+        expect2(await manager.prepare('2', {}, true).catch(GETERR), '!_id').toEqual({
+            ns: 'TT',
+            type: 'user',
+            id: '2',
+            active: 1,
+            createdAt: current,
+            updatedAt: current,
+            deletedAt: 0,
+        });
     });
 
-    it('should pass inserting new model(s)', async done => {
+    it('should pass inserting new model(s)', async () => {
         const { manager, current } = instance();
         // model validation failed (in onBeforeSave())
         expect2(await manager.insert({}).catch(GETERR)).toEqual('.uid is required!');
         expect2(await manager.insert({ uid: 'U0002' }).catch(GETERR)).toEqual('.name is required!');
         // success
-        expect2(await manager.insert({ uid: 'U0002', name: '임꺽정' }).catch(GETERR), '!_id,!id').toEqual({ ns: 'TT', type: 'user', uid: 'U0002', name: '임꺽정', active: 1, createdAt: current, updatedAt: current, deletedAt: 0 });
-        expect2(await manager.insert({ uid: 'U0003', name: '전봉준', active: 0 }).catch(GETERR), '!_id,!id').toEqual({ ns: 'TT', type: 'user', uid: 'U0003', name: '전봉준', active: 0, createdAt: current, updatedAt: current, deletedAt: 0 });
-        done();
+        expect2(await manager.insert({ uid: 'U0002', name: '임꺽정' }).catch(GETERR), '!_id,!id').toEqual({
+            ns: 'TT',
+            type: 'user',
+            uid: 'U0002',
+            name: '임꺽정',
+            active: 1,
+            createdAt: current,
+            updatedAt: current,
+            deletedAt: 0,
+        });
+        expect2(await manager.insert({ uid: 'U0003', name: '전봉준', active: 0 }).catch(GETERR), '!_id,!id').toEqual({
+            ns: 'TT',
+            type: 'user',
+            uid: 'U0003',
+            name: '전봉준',
+            active: 0,
+            createdAt: current,
+            updatedAt: current,
+            deletedAt: 0,
+        });
     });
 
-    it('should pass updating existing model(s)', async done => {
+    it('should pass updating existing model(s)', async () => {
         const { manager } = instance();
         expect2(await manager.retrieve('1').catch(GETERR), 'age,active').toEqual({ age: 32, active: 1 });
         // update field
@@ -144,14 +182,15 @@ describe('ModelManager', () => {
         expect2(await manager.update('1', { active: 1 }, { age: 1 }), 'age,active').toEqual({ age: 34, active: 1 });
         expect2(await manager.retrieve('1').catch(GETERR), 'age,active').toEqual({ age: 34, active: 1 });
         // wrong increment field
-        expect2(await manager.update('1', null, { name: '오함마' }).catch(GETERR), 'name').toBe(`.name (오함마) should be number!`);
+        expect2(await manager.update('1', null, { name: '오함마' }).catch(GETERR), 'name').toBe(
+            `.name (오함마) should be number!`,
+        );
         // not existing model
         expect2(await manager.retrieve('2').catch(GETERR)).toBe('404 NOT FOUND - user:2');
         expect2(await manager.update('2', { active: 1 }).catch(GETERR)).toBe('404 NOT FOUND - user:2');
-        done();
     });
 
-    it('should pass updating or inserting model(s)', async done => {
+    it('should pass updating or inserting model(s)', async () => {
         const { manager, current } = instance();
         expect2(await manager.retrieve('1').catch(GETERR), 'age,active').toEqual({ age: 32, active: 1 });
         // update field
@@ -161,32 +200,70 @@ describe('ModelManager', () => {
         expect2(await manager.updateOrCreate('1', null, { age: 1 }), 'age,active').toEqual({ age: 33, active: 0 });
         expect2(await manager.retrieve('1').catch(GETERR), 'age,active').toEqual({ age: 33, active: 0 });
         // both update and increment
-        expect2(await manager.updateOrCreate('1', { active: 1 }, { age: 1 }), 'age,active').toEqual({ age: 34, active: 1 });
+        expect2(await manager.updateOrCreate('1', { active: 1 }, { age: 1 }), 'age,active').toEqual({
+            age: 34,
+            active: 1,
+        });
         expect2(await manager.retrieve('1').catch(GETERR), 'age,active').toEqual({ age: 34, active: 1 });
         // wrong increment field
-        expect2(await manager.update('1', null, { name: '오함마' }).catch(GETERR), 'name').toBe(`.name (오함마) should be number!`);
+        expect2(await manager.update('1', null, { name: '오함마' }).catch(GETERR), 'name').toBe(
+            `.name (오함마) should be number!`,
+        );
         // not existing model
-        const expected = { ns: 'TT', type: 'user', uid: 'U0002', name: '오함마', active: 0, createdAt: current, updatedAt: current, deletedAt: 0 };
+        const expected = {
+            ns: 'TT',
+            type: 'user',
+            uid: 'U0002',
+            name: '오함마',
+            active: 0,
+            createdAt: current,
+            updatedAt: current,
+            deletedAt: 0,
+        };
         expect2(await manager.retrieve('2').catch(GETERR)).toBe('404 NOT FOUND - user:2');
-        expect2(await manager.updateOrCreate('2', { uid: 'U0002', name: '오함마', active: 0 }).catch(GETERR), '!_id,!id').toEqual(expected);
+        expect2(
+            await manager.updateOrCreate('2', { uid: 'U0002', name: '오함마', active: 0 }).catch(GETERR),
+            '!_id,!id',
+        ).toEqual(expected);
         expect2(await manager.retrieve('2').catch(GETERR), '!_id,!id').toEqual(expected);
-        done();
     });
 
-    it('should pass deleting existing model(s)', async done => {
+    it('should pass deleting existing model(s)', async () => {
         const { manager, current } = instance();
         // hard-delete
-        expect2(await manager.insert({ uid: 'U0002', name: '김두한' }).catch(GETERR), 'id,deletedAt').toEqual({ id: '1000001', deletedAt: 0 });
-        expect2(await manager.retrieve('1000001').catch(GETERR), 'id,deletedAt').toEqual({ id: '1000001', deletedAt: 0 });
-        expect2(await manager.delete('1000001', true).catch(GETERR), 'id,deletedAt').toEqual({ id: '1000001', deletedAt: 0 });
-        expect2(await manager.retrieve('1000001').catch(GETERR), 'id, deletedAt').toEqual('404 NOT FOUND - user:1000001');
+        expect2(await manager.insert({ uid: 'U0002', name: '김두한' }).catch(GETERR), 'id,deletedAt').toEqual({
+            id: '1000001',
+            deletedAt: 0,
+        });
+        expect2(await manager.retrieve('1000001').catch(GETERR), 'id,deletedAt').toEqual({
+            id: '1000001',
+            deletedAt: 0,
+        });
+        expect2(await manager.delete('1000001', true).catch(GETERR), 'id,deletedAt').toEqual({
+            id: '1000001',
+            deletedAt: 0,
+        });
+        expect2(await manager.retrieve('1000001').catch(GETERR), 'id, deletedAt').toEqual(
+            '404 NOT FOUND - user:1000001',
+        );
         // soft-delete
-        expect2(await manager.insert({ uid: 'U0003', name: '이만기' }).catch(GETERR), 'id,deletedAt').toEqual({ id: '1000002', deletedAt: 0 });
-        expect2(await manager.retrieve('1000002').catch(GETERR), 'id,deletedAt').toEqual({ id: '1000002', deletedAt: 0 });
-        expect2(await manager.delete('1000002', false).catch(GETERR), 'id,deletedAt').toEqual({ id: '1000002', deletedAt: current });
-        expect2(await manager.retrieve('1000002').catch(GETERR), 'id,deletedAt').toEqual({ id: '1000002', deletedAt: current });
+        expect2(await manager.insert({ uid: 'U0003', name: '이만기' }).catch(GETERR), 'id,deletedAt').toEqual({
+            id: '1000002',
+            deletedAt: 0,
+        });
+        expect2(await manager.retrieve('1000002').catch(GETERR), 'id,deletedAt').toEqual({
+            id: '1000002',
+            deletedAt: 0,
+        });
+        expect2(await manager.delete('1000002', false).catch(GETERR), 'id,deletedAt').toEqual({
+            id: '1000002',
+            deletedAt: current,
+        });
+        expect2(await manager.retrieve('1000002').catch(GETERR), 'id,deletedAt').toEqual({
+            id: '1000002',
+            deletedAt: current,
+        });
         // not existing model
         expect2(await manager.delete('1000003').catch(GETERR)).toEqual('404 NOT FOUND - user:1000003');
-        done();
     });
 });
