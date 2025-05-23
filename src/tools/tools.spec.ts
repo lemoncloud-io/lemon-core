a42c9720419567794fd8947e6aeaef244086c9f5
Claire <claire@lemoncloud.io>
2025-05-23 14:15:14 +0900

    test: add unit test for awsConfig function to validate AWS credentials handling

--- src/tools/tools.spec.ts
+++ src/tools/tools.spec.ts
@@ -8,7 +8,10 @@
  *
  * @copyright (C) lemoncloud.io 2019 - All Rights Reserved.
  */
-import { loadJsonSync } from './tools';
+import { fromIni } from '@aws-sdk/credential-providers';
+import { expect2, GETERR } from '../common/test-helper';
+import { loadJsonSync, awsConfig } from './tools';
+import $engine from '../engine';
 
 //! main test body.
 describe('Test tools/shared', () => {
@@ -18,5 +21,28 @@ describe('Test tools/shared', () => {
         expect(data1.name).toEqual('lemon-core');
         expect(data2.name).toEqual('lemon-core');
         expect(data1.version).toEqual(data2.version);
+    });
+
+    it('test awsConfig()', async () => {
+        const region = 'ap-northeast-2';
+        const profile = $engine.environ('NAME', 'none') as string;
+        const cfg = awsConfig(region);
+        expect2(() => cfg.region).toEqual(region);
+        expect2(() => typeof cfg.credentials).toEqual('function');
+
+        if (profile !== 'none') {
+            // get credentials
+            const expectedCreds = await fromIni({ profile })();
+            const creds = typeof cfg.credentials === 'function' ? await cfg.credentials() : undefined;
+
+            expect2(creds?.accessKeyId).toEqual(expectedCreds.accessKeyId);
+            expect2(creds?.secretAccessKey).toEqual(expectedCreds.secretAccessKey);
+            expect2(creds?.sessionToken).toEqual(expectedCreds.sessionToken);
+        } else {
+            const creds = typeof cfg.credentials === 'function' ? await cfg.credentials().catch(GETERR) : null;
+            expect2(creds).toEqual(
+                'Could not resolve credentials using profile: [none] in configuration/credentials file(s).',
+            );
+        }
     });
 });
