00e6b86e93683779c092a33046cbedcdccf99682
Steve Jung <steve@lemoncloud.io>
2025-08-08 17:01:28 +0900

    v4.0.6

--- src/tools/express.spec.ts
+++ src/tools/express.spec.ts
@@ -88,7 +88,7 @@ describe('express', () => {
         if (!hasDevkit) return; // ignore.
 
         const $pack = loadJsonSync('package.json');
-        expect2(() => $pack.devDependencies['lemon-devkit']).toEqual('^0.0.3');
+        expect2(() => $pack.devDependencies['lemon-devkit']).toEqual('^0.0.6');
 
         const { $express } = instance();
         const $res = $express();
