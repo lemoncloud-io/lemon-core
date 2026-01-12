c33c9e8531b09198052ce0b9caaa22ea72c6d305
Steve Jung <steve@lemoncloud.io>
2026-01-12 18:13:13 +0900

    v4.1.9 released

--- src/cores/storage/storage-service.ts
+++ src/cores/storage/storage-service.ts
@@ -39,7 +39,7 @@ export interface StorageService<T extends StorageModel> {
      *
      * @param ids       ids
      */
-    mread(ids: string[]): Promise<BatchResult<T>>;
+    mread?(ids: string[]): Promise<BatchResult<T>>;
 
     /**
      * read or create if not-found.
@@ -73,7 +73,7 @@ export interface StorageService<T extends StorageModel> {
      *
      * @param ids       ids
      */
-    mupdate(list: T[]): Promise<BatchResult<T>>;
+    mupdate?(list: T[]): Promise<BatchResult<T>>;
 
     /**
      * increment number attribute (or overwrite string field)
@@ -99,7 +99,7 @@ export interface StorageService<T extends StorageModel> {
 import { DynamoService, KEY_TYPE, BatchResult, FailedItem } from '../dynamo/';
 import { loadDataYml } from '../../tools/';
 
-interface MyGeneral extends GeneralItem, StorageModel { }
+interface MyGeneral extends GeneralItem, StorageModel {}
 
 const clearDuplicated = (arr: string[]) =>
     arr.sort().reduce((L, val) => {
