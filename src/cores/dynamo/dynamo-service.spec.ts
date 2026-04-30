5fb785860520f34c8a0808e68958313598b2d5a9
Steve Jung <steve@lemoncloud.io>
2026-04-30 17:22:35 +0900

    chore: comment

--- src/cores/dynamo/dynamo-service.spec.ts
+++ src/cores/dynamo/dynamo-service.spec.ts
@@ -8,6 +8,7 @@
  *
  * @copyright (C) 2019 LemonCloud Co Ltd. - All Rights Reserved.
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { describe, expect, it, vi } from 'vitest';
 import { loadProfile } from '../../environ';
 import { GETERR, expect2, _it } from '../../common/test-helper';
